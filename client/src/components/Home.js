fa35fd42e2941c0cbfc9b692a84d448b54369854
David <dcrossmanagement@gmail.com>
2020-02-03 18:35:58 -0500

    formatted home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,7 +7,7 @@ class Home extends React.Component {
                 <h1>Rick and Morty Character Search</h1>
                 <h2>Welcome to the Home Of All The Rick and Morty Characters In All The Known Universes!</h2>
                 <img src="/images/sample.jpg" alt="landing page img"/>
-                <h3>Use the Search Page to look for your favorite Rick and Morty Characters and learn more about each of them!</h3>
+                <h3>Use the Characters Page to search for more info on your favorite character, the Locations Page to see the residents of each place Rick and Morty have ever been, and the Episodes Page to see which characters were in each episode!</h3>
             </div>
         )
     }
