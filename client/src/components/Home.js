01c54599a786f555bdf29650bcd1185a45bd8d99
David <dcrossmanagement@gmail.com>
2022-11-06 19:48:39 -0500

    Changes made to home page to confirm ability to make changes

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,10 +4,10 @@ class Home extends React.Component {
     render() {
         return (
             <div id="home-page">
-                <h1>Rick and Morty Character Search</h1>
-                <h2>Welcome to the Home Of All The Rick and Morty Characters In All The Known Universes!</h2>
+                <h1>Banana Bears Trading Club</h1>
+                <h2>Welcome to the webpage for the market monitor dashboards!</h2>
                 <img src="/images/sample.jpg" alt="landing page img"/>
-                <h3>Use the Characters Page to search for more info on your favorite character, the Locations Page to see the residents of each place Rick and Morty have ever been, and the Episodes Page to see which characters were in each episode!</h3>
+                <h3>Find subsequent instructions here!</h3>
             </div>
         )
     }
